a09b5ffbd96a253badcb31ca785c8e80cdfe6dc3
Rishit Kundariya <60743923+RishitKundariya@users.noreply.github.com>
2022-01-07 10:15:20 +0530

    change in design of user management

--- HTML/assests/js/main.js
+++ HTML/assests/js/main.js
@@ -65,3 +65,77 @@ $(function(){
         }
     });
 });
+$(document).ready(function() {
+    $('#adminServiceRequest').dataTable({
+        "bFilter": false,
+        "bInfo": false,
+        'aoColumnDefs': [{
+            'bSortable': false,
+            'aTargets': [-1] /* 1st one, start by the right */
+        }]
+    });
+  
+});
+$(function(){
+    $.contextMenu({
+        selector: '.context-menu-new', 
+        trigger: 'left',
+        callback: function(key, options) {
+        },
+        items: {
+            "edit": {name: "Edit & Rechedule" },
+            "inquiry": {name: "Inquiry"},
+            "history": {name: "History Log"},
+            "download": {name: "Download Invoice"},
+            "transaction": {name: "Other Transaction"},
+        }
+    });
+});
+$(function(){
+    $.contextMenu({
+        selector: '.context-menu-pending', 
+        trigger: 'left',
+        callback: function(key, options) {
+        },
+        items: {
+            "edit": {name: "Edit & Rechedule" },
+            "refund": {name: "Refund"},
+            "cancle": {name: "Cancle"},
+            "changesp": {name: "Change SP"},
+            "escalate": {name: "Escalate"},
+            "history": {name: "History Log"},
+            "download": {name: "Download Invoice"}
+         
+        }
+    });
+});
+$(function(){
+    $.contextMenu({
+        selector: '.context-menu-completed', 
+        trigger: 'left',
+        callback: function(key, options) {
+        },
+        items: {
+            "refund": {name: "Refund"},
+            "escalate": {name: "Escalate"},
+            "history": {name: "History Log"},
+            "download": {name: "Download Invoice"}
+        }
+    });
+});
+$(function(){
+    $.contextMenu({
+        selector: '.context-menu-cancel', 
+        trigger: 'left',
+        callback: function(key, options) {
+        },
+        items: {
+            "edit": {name: "Edit & Rechedule" },
+            "refund": {name: "Refund"},
+            "inquiry": {name: "Inquiry"},
+            "history": {name: "History Log"},
+            "download": {name: "Download Invoice"},
+            "transaction": {name: "Other Transaction"},
+        }
+    });
+});
